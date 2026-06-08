5bb8c61ebdaf337cd39c6eb3eadfaa8d2ad48a33
Markus <60819916@continental.edu.pe>
2026-06-07 19:56:28 -0500

    Agregar funciones de pila (stack) para devoluciones

--- BibliotecaUniversitaria.cpp
+++ BibliotecaUniversitaria.cpp
@@ -123,9 +123,55 @@ struct devolucion{
 	devolucion *sig;
 };
 
+//FUNCIONES PARA PILA (Stack de devoluciones)
+void insertarDevolucion(devolucion *&tope, string libro){
+	devolucion *nuevo = new devolucion;
+	
+	nuevo->libro = libro;
+	nuevo->sig = tope;
+	
+	tope = nuevo;
+}
+
+void mostrarDevoluciones(devolucion *tope){
+	devolucion *aux = tope;
+	
+	cout<<"\nDEVOLUCIONES REGISTRADAS\n\n";
+	
+	while(aux != NULL){
+		cout<<"------------------\n";
+		cout<<"Libro: "<<aux->libro<<endl;
+		cout<<"------------------\n";
+		
+		aux = aux->sig;
+	}
+}
+
+void extraerDevolucion(devolucion *&tope){
+	if(tope == NULL){
+		cout<<"No hay devoluciones registradas\n";
+		return;
+	}
+	
+	devolucion *aux = tope;
+	cout<<"Devolucion extraida: "<<tope->libro<<endl;
+	tope = tope->sig;
+	delete aux;
+}
+
+void vaciarPila(devolucion *&tope){
+	while(tope != NULL){
+		devolucion *aux = tope;
+		tope = tope->sig;
+		delete aux;
+	}
+	cout<<"Pila vaciada correctamente\n";
+}
 
 int main(){
 	Libro *lista = NULL;
+	devolucion *tope = NULL;
+	
 	//MENU GLOBAL
     int op;
     int subop;
@@ -193,19 +239,26 @@ int main(){
                 menudevolucion();
                 cout<<"Ingrese opcion: ";
                 cin>>subop;
+                switch(subop){
+                	case 1:{
+                		string libro;
+                		cin.ignore();
+                		cout<<"Ingrese nombre del libro a devolver: ";
+                		getline(cin, libro);
+                		insertarDevolucion(tope, libro);
+                		cout<<"Devolucion registrada\n";
+                		break;
+					}
+					case 2:{
+						mostrarDevoluciones(tope);
+						break;
+					}
+				}
             }while(subop!=0);
             break;
         }
     }while (op!=0);
+    
+    vaciarPila(tope);
     return 0;
 }
-	
-	//lista enlazada
-	
-	
-	//cola
-	//estudiante *frente = NULL;
-	//estudiante *anterior = NULL;
-	
-	//pila
-	//devolucion *tope = NULL;
